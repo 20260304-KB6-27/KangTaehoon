d6bc6241dacd913fb8554010761a522b41633b4a
Charles <rhcharles0@gmail.com>
2026-03-24 12:42:11 +0900

    feat: 15일차 단일컴포넌트 basic tasks

---
+++ 03_Vue/01_basic/06_단일파일컴포넌트/01_test-vue-app/src/components/CheckboxItem2.vue
@@ -0,0 +1,15 @@
+<template>
+    <li><input type="checkbox" v-model="idol.checked" />{{ idol.name }}</li>
+</template>
+
+<script>
+export default {
+    name: 'CheckboxItem2',
+    props: ['idol'],
+    data() {
+        return {
+            checked: false,
+        };
+    },
+};
+</script>
